f015f42ef4ae963f532959dc88dceb78b848cbc2
khushal patil <khushalpatil260@gmail.com>
2024-10-08 23:30:13 +0530

    solve the problem

---
+++ polindrone.java
@@ -0,0 +1,21 @@
+/**
+ * polindrone
+ */
+public class polindrone {
+
+    public static boolean ispolindrone(String str){
+           for(int i=0;i<str.length()/2;i++){
+            int n=str.length();
+            if(str.charAt(i) != str.charAt(n-1-i)){
+                return false;
+            }
+
+           }
+           return true;
+    }
+
+    public static void main(String[] args) {
+        String str="khsuhal";
+        System.out.println(ispolindrone(str));
+    }
+}
